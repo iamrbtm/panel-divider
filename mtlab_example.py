949dcb2c8f5b15181cb3520ac5121597a3d11478
iamrbtm <rbtm2006@me.com>
2021-03-17 01:40:27 -0700

    testing

--- mtlab_example.py
+++ mtlab_example.py
@@ -1,18 +1,21 @@
 import panelDivider as pd
-from PIL import Image, ImageDraw, ImageFont
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 # TODO: Add a way to bring panels in from a db
 # TODO: bring boards and panels in from a csv 
 boards = []
 boards.append(pd.Board(width=20, length=40, thickness=.5, name='Rside'))
 boards.append(pd.Board(width=20, length=40, thickness=.5, name='Lside'))
-boards.append(pd.Board(width=20, length=30, thickness=.75, name='Front'))
-boards.append(pd.Board(width=20, length=30, thickness=.75, name='Back'))
+boards.append(pd.Board(width=20, length=30, thickness=.5, name='Front'))
+boards.append(pd.Board(width=20, length=30, thickness=.5, name='Back'))
+boards.append(pd.Board(width=2, length=5, thickness=.5, name='smTest'))
+boards.append(pd.Board(width=5, length=7, thickness=.5, name='test'))
 
 
 panels = []
 panels.append(pd.Board(width=48,length=96,thickness=.5,name="half inch plywood"))
-panels.append(pd.Board(width=48, length=96, thickness=.75, name="three quearter plywood"))
+# panels.append(pd.Board(width=48, length=96, thickness=.5, name="three quearter plywood"))
 
 div = pd.Divider(boards, panels, 0)
 
@@ -25,16 +28,14 @@ for item in output:
         panelsUsed.append(item[3])
         
 for panel in panelsUsed:
-    im = Image.new('RGB', ((panels[0].length), (panels[0].width)), (128, 128, 128))
-    draw = ImageDraw.Draw(im)
+    fig, ax = plt.subplots()
+    plt.axis([0,48,0,96])
     
     for o in output:
         if o[3] == panel:
             print("Board \"" + o[1] + "\" #" + str(o[2]) + " on panel \"" + o[3] + "\" #" + str(o[4]) + " at (" + str(o[0].x) + ", " + str(o[0].y) + ", " + str(o[0].w) +", " + str(o[0].h) +")")
 
-            draw.rectangle([(o[0].x, o[0].y), (o[0].h, o[0].h)], fill=(0, 192, 192), outline=(255, 255, 255))
-            # font = ImageFont.load_default()
-            # draw.text((o[0].x+2, o[0].y+2), str(o[0].h) + "," + str(o[0].w), font=font)
+            ax.add_patch(Rectangle((o[0].x, o[0].y), o[0].w, o[0].h,EdgeColor='red'))
 
-    im.save(panel+'_imagedraw.jpg', quality=95)
-    im.show()
+    print('-dpdf',panel+'_imagedraw.jpg')
+    
